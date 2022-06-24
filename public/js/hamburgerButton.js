99a77e029e02652f7f44a75564fe7f99b7239807
matiscava <matiscava@hotmail.com>
2022-06-24 16:24:40 -0300

    Le puse color similar a la web

--- public/js/hamburgerButton.js
+++ public/js/hamburgerButton.js
@@ -4,11 +4,13 @@ export default function hamburgerMenu ( panelBtn , panel , menuLink) {
     if( e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`) ) {
       d.querySelector(panel).classList.toggle('is-active')
       d.querySelector(panelBtn).classList.toggle('is-active')
+      d.querySelector('body').classList.toggle('not-scroll')
     }
 
     if( e.target.matches(menuLink)) {
       d.querySelector(panel).classList.remove('is-active')
       d.querySelector(panelBtn).classList.remove('is-active')
+      d.querySelector('body').classList.toggle('not-scroll')
     }
   })
 }
