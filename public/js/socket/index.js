b3870d68ad09b30fe70891e01660ad58d45fdef5
matiscava <matiscava@hotmail.com>
2022-08-12 16:40:18 -0300

    Vamos a ver si ahora lee cuando se agranda la pantalla

--- public/js/socket/index.js
+++ public/js/socket/index.js
@@ -1,6 +1,8 @@
 // import { io } from "../../../main.js";
 import NewNotification from "./NewNotification.js";
 import NewWarn from "./NewWarn.js";
+import NotificationBtn from "./NotificationBtn.js";
+import NotificationIcon from "./NotificationIcon.js";
 import NotificationResponsive from "./NotificationResponsive.js";
 
 const socket = io.connect();
@@ -13,7 +15,9 @@ socket
     NewWarn(data);
   })
   .on('get-notifications', (data) => {
-    NotificationResponsive(data);
+    // NotificationResponsive(data);
+    NotificationBtn(data);
+    NotificationIcon(data);
   })
 
   
