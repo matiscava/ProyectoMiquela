67c0f4bc4c2b51fd610d833a67b83210c8c6be19
matiscava <matiscava@hotmail.com>
2022-08-12 16:35:59 -0300

    Vamos a ver si ahora lee cuando se agranda la pantalla

--- public/js/socket/NotificationResponsive.js
+++ public/js/socket/NotificationResponsive.js
@@ -2,16 +2,35 @@ import NotificationBtn from "./NotificationBtn.js";
 import NotificationIcon from "./NotificationIcon.js";
 
 function NotificationResponsive (data) {
-  if(window.innerWidth > 1150) {
+  const originalHeight = window.innerHeight;
+  const originalWidth = window.innerWidth;
+
+  if(originalWidth > 1150) {
     NotificationIcon(data)
   }else{
     NotificationBtn(data)
   }
   window.addEventListener('resize', (e) => {
-    if(window.innerWidth > 1150) {
-      NotificationIcon(data)
-    }else{
-      NotificationBtn(data)
+
+    if (originalHeight != window.innerHeight) {
+      // console.log('cambio');
+      if(window.innerWidth > 1150) {
+        // console.log('compu');
+        NotificationIcon(data)
+      }else {
+        // console.log('celu');
+        NotificationBtn(data)
+      }
+    }
+    if (originalWidth != window.innerWidth) {
+      // console.log('cambio ancho');
+      if(window.innerWidth > 1150) {
+        // console.log('compu');
+        NotificationIcon(data)
+      }else {
+        // console.log('celu');
+        NotificationBtn(data)
+      }
     }
   })
 }
